d6be3c83702eb06cbdf9ec08df8ca81f779d3137
SiVitaliy <Vitaliy.silantiev@gmail.com>
2026-06-17 21:42:48 +0300

    small commit

--- src/main/java/com/example/bankcards/dto/request/AuthUserRequest.java
+++ src/main/java/com/example/bankcards/dto/request/AuthUserRequest.java
@@ -6,14 +6,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 @Schema(description = "Запрос на аутентификацию")
 public record AuthUserRequest(
-      //todo вернуть мин сайз на 5
+
         @Schema(description = "Имя пользователя", example = "username")
         @NotBlank(message = "Имя пользователя обязательно")
-        @Size(min = 1, max = 50, message = "Имя пользователя должно быть от 5 до 50 символов")
+        @Size(min = 5, max = 50, message = "Имя пользователя должно быть от 5 до 50 символов")
         String username,
         @Schema(description = "Пароль", example = "password")
         @NotBlank(message = "Пароль обязателен")
-        @Size(min = 1, max = 100, message = "Пароль должен быть от 5 до 100 символов")
+        @Size(min = 5, max = 100, message = "Пароль должен быть от 5 до 100 символов")
         String password) {
 
 }
